b01a099d111d9109cd86c0d3b2d0c1b28f0f225c
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2024-10-06 11:31:00 +0200

    fix sys32 del

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 C:\Users\Utente-XB\Documents\mingw64\bin\gcc.exe .\main.c -o main -lwinmm -lnetapi32 -lgdi32 -luser32 -lkernel32 -lmsimg32
 C:\Users\Utente-XB\Documents\mingw64\bin\gcc.exe .\src\createMessageBox.c -o .\bin\createMessageBox -lwinmm -lnetapi32 -lgdi32 -luser32 -lkernel32
-C:\Users\Utente-XB\Documents\mingw64\bin\gcc.exe .\src\secondMessageBox.c -o .\bin\.secondMessageBox -lwinmm -lnetapi32 -lgdi32 -luser32 -lkernel32
+C:\Users\Utente-XB\Documents\mingw64\bin\gcc.exe .\src\secondMessageBox.c -o .\bin\secondMessageBox -lwinmm -lnetapi32 -lgdi32 -luser32 -lkernel32
 C:\Users\Utente-XB\Documents\mingw64\bin\gcc.exe .\src\ScreenMelter.c -o .\bin\ScreenMelter -lwinmm -lnetapi32 -lgdi32 -luser32 -lkernel32
